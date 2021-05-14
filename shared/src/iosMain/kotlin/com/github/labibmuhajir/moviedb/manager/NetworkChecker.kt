cb85d5b014ce9af37869c73205bd55ea1f859dcd
labib muhajir <labibmuhajir@yahoo.com>
2021-05-14 17:48:38 +0700

    add notes on NetoworkChecker iosMain

--- shared/src/iosMain/kotlin/com/github/labibmuhajir/moviedb/manager/NetworkChecker.kt
+++ shared/src/iosMain/kotlin/com/github/labibmuhajir/moviedb/manager/NetworkChecker.kt
@@ -1,13 +1,15 @@
 package com.github.labibmuhajir.moviedb.manager
 
-import platform.SystemConfiguration.SCNetworkReachabilityCreateWithAddress
-
-
 actual class NetworkChecker {
     actual fun isNetworkAvailable(): Boolean {
-        //todo
-
-
         return true
+        /***
+         * add following dependency on gradle
+         * cocoapods {
+         *  pod("FXReachability")
+         * }
+         * then in this actual class use FXReachability.sharedInstance().reachable
+         * swift ui preview not working must run on ios or ipados devices
+        ***/
     }
 }

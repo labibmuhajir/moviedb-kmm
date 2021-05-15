4dd731e514f446612f47ad767cb834758eb13404
labib muhajir <labibmuhajir@yahoo.com>
2021-05-15 17:43:57 +0700

    feature-4-theming-ios add background and app color

---
+++ iosApp/iosApp/Constants/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  iosApp
+//
+//  Created by ITMI on 15/05/21.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import Foundation
+import BlackLabsColor
+import UIKit
+import shared
+import SwiftUI
+
+    extension Color {
+        static let primaryColor = Color(UIColor.init(hex: AppColors.init().primaryColor))
+        static let backgroundColor = Color(UIColor.init(hex: AppColors.init().backgroundColor))
+        static let onBackgroundColor = Color(UIColor.init(hex: AppColors.init().onBackgroundColor))
+    }
